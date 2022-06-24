d712968ca69f86809b00d5b257959873a59cdef7
IsaGalo80 <isagalo1980@outlook.com>
2022-06-24 20:29:29 +0000

    falta hacer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import  ReactDOM  from "react-dom";
+import "../../styles/index.css"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, brillo]=useState(null);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div className="palito">
+				<div className={ color==="red"? "red luzenc": "red"}   onClick = {()=>brillo("red")}></div>
+				<div className={ color==="yellow"? "yellow luzenc": "yellow"}onClick = {()=>brillo("yellow")}></div>
+				<div className={ color==="green"? "green luzenc": "green"}onClick = {()=>brillo("green")}></div>
+			</div>
 		</div>
-	);
-};
+	)
+		}
+
 
 export default Home;
+ReactDOM.render(<Home />, document.querySelector('#app'));
